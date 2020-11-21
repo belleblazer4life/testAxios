ed1cc15db625b13a01477038fc8e876d48e8edb2
belleblazer4life <belleblazer4life@gmail.com>
2020-11-21 01:53:28 +0100

    changed URL to covid api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
-//const url ="https://api.apify.com/v2/key-value-stores/vqnEUe7VtKNMqGqFF/records/LATEST?disableRedirect=true";
+const url ="https://api.apify.com/v2/key-value-stores/vqnEUe7VtKNMqGqFF/records/LATEST?disableRedirect=true";
 
-const url = "https://www.tvgids.nl/gids/spike";
+//const url = "https://www.tvgids.nl/gids/spike";
 const getData = () => {
   axios
     .get(url)
