6b8a230e5170af304677621fe5b8c3c9e15faaab
belleblazer4life <belleblazer4life@gmail.com>
2020-11-21 02:21:59 +0100

    added a command line before API URL

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-const axios = require("axios");
-
-const url ="https://api.apify.com/v2/key-value-stores/vqnEUe7VtKNMqGqFF/records/LATEST?disableRedirect=true";
+const axios = require('axios');
+//Covid-19 API
+const url =
+  'https://api.apify.com/v2/key-value-stores/vqnEUe7VtKNMqGqFF/records/LATEST?disableRedirect=true';
 
 //const url = "https://www.tvgids.nl/gids/spike";
 const getData = () => {
